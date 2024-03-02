5b5eb61bfc99f4754cb37ac0c9fdf6bcb9aa52df
Seymap36 <161921264+Seymap36@users.noreply.github.com>
2024-03-02 12:03:46 +0300

    Deneme92

---
+++ deneme.py
@@ -0,0 +1,15 @@
+## print komutu/fonksiyonu içinde tek, çift ve üç tırnak kullanılabilir:
+
+print ( 'Merhaba')
+
+print ("Nasılsın?")
+
+print ('''Nasılsınız?''')
+
+#print komutunda dıştaki tırnaklar içte kullanı
+#print ('Eren'in annesi "gel" dedi')
+#\ veya dışta üç tırnak ile yapılabilir.
+print('''Eren'in Annesi "gel" dedi''')
+print('Eren\'in annesi "gel" dedi')
+print('Metin Şeyma\'ya sürekli trip atıyor')
+print('Doja\'nın dogum günü "15.04.2023"')
