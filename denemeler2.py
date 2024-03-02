e17a63777c612ee9a3c5d78b4e36d924d75b947b
Seymap36 <161921264+Seymap36@users.noreply.github.com>
2024-03-02 13:03:10 +0300

    Add files via upload

---
+++ denemeler2.py
@@ -0,0 +1,16 @@
+## string ve numeric ifadeler ve değişken ifadeleri
+
+print("Doja", "Şeyma")
+print("Doja"+ "Şeyma")
+print("Doja", 36)
+print("Doja"+ "36")
+print("Doja "*5)
+
+metin = 12
+seyma = 13
+print(metin+ seyma)
+print("metin"+ "seyma")
+
+print("\n")
+print('\nPazartesi', 'Salı')
+print('Seyma Pırım\n7B\n439')
