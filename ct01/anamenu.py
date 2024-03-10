13ed74b2403c352a9c9a4cced58390b2d6364cff
Seymap36 <seyma.priim123@gmail.com>
2024-03-10 13:21:49 +0300

    deneme cizim

--- ct01/anamenu.py
+++ ct01/anamenu.py
@@ -1,5 +1,6 @@
 import hesapmak.hm
 import nothesabi.nothesaplayici
+import cizimler.cizim
 
 def ANAMENU():
     print("╔═════════════════════╗")
@@ -7,7 +8,7 @@ def ANAMENU():
     print("║                     ║")
     print("║  1-HESAP MAK        ║")
     print("║  2-KM HESAP         ║")
-    print("║  3-                 ║")
+    print("║  3-Cizim            ║")
     print("║  4-Not hesabı       ║")
     print("║  ç-çıkış            ║")
     print("║                     ║")
@@ -16,6 +17,9 @@ def ANAMENU():
     seçim = input()
     if seçim == "1" :
         hesapmak.hm.hmmenu()
+        ANAMENU()
+    if seçim == "3" :
+        cizimler.cizim.cizimlermenu()
         ANAMENU()
     if seçim == "ç" or seçim=="ç": exit()
     if seçim == "4":
---
+++ ct01/cizimler/cizim.py
@@ -0,0 +1,23 @@
+import turtle 
+def ucgen():
+    for abc in range(3):
+        turtle.forward(100)
+        turtle.right(120)
+
+def kare():
+    for a in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+def cizimlermenu():
+    print("╔═══════════════════════════════════╗")
+    print("║        Cizimler                   ║")
+    print("║ 1-ucgen                           ║")
+    print("║ 2-kare                            ║")
+    print("║ 3-                                ║")
+    print("║ 4-                                ║")
+    print("║          Seçiminiz Nedir?         ║")
+    print("╚═══════════════════════════════════╝")
+
+secim = input()
+if secim == "1" : ucgen()
+if secim == "2" : kare()
